0e3de050fbd975a9ca366da1a7f871a84e488d2e
lopiga05 <lopiga05@gmail.com>
2020-11-22 19:58:45 +0700

    projek

--- tiketpsawat.py
+++ tiketpsawat.py
@@ -12,7 +12,7 @@ def print_menu():
     [4]. Batalkan/Hapus Tiket Pesawat
     [5]. Cetak PDF Tiket Pesawat
     [6]. Cetak QR Tiket Pesawat
-    [Q]. Keluar
+    [Q]. Tentang
         """)
  
 def print_header(msg):
@@ -55,7 +55,7 @@ def print_data(id_pesanan=None, judul=True, jam=True, tanggal=True, studio=True,
             kursi = tickets[id_pesanan]["kursi"]
             print(f"NOMOR PESANAN : {id_pesanan} - JUDUL : {judul} - JAM : {jam} - TANGGAL : {tanggal} - GATE : {gate} - NOMOR KURSI : {kursi}")
  
-def view_contacts():
+def view_ticket():
     print_header("DAFTAR TIKET TERSIMPAN")
     if not_empty(tickets):
         print_data(all_data=True)
@@ -123,7 +123,7 @@ def delete_ticket():
     exists = searching_by_id(nopes)
     if exists:
         print_data(contact=exists)
-        respon = input(f"Yakin ingin menghapus {id_pesanan} ? (Y/N) ")
+        respon = input(f"Yakin ingin menghapus {id_ticket} ? (Y/N) ")
         if verify_ans(respon):
             del tickets[id_pesanan]
             saved = save_data_tickets()
@@ -149,7 +149,7 @@ def update_ticket_nama(id_ticket):
     else:
         print("Data Batal diubah")
  
-def update_ticket_jam(id_contact):
+def update_ticket_jam(id_ticket):
     print(f"Ticket Jam Lama : {contacts[id_contact]['telp']}")
     new_telp = input("Masukkan Ticket Jam Baru : ")
     respon = input("Apakah yakin data ingin diubah (Y/N) : ")
@@ -162,7 +162,7 @@ def update_ticket_jam(id_contact):
     else:
         print("Data Batal diubah")
  
-def update_ticket_tanggal(contact):
+def update_ticket_tanggal(ticket):
     print(f"Tanggal Lama : {ticket}")
     new_date = input("Masukkan Tanggal baru : ")                  
     respon = input("Apakah yakin data ingin diubah (Y/N) : ")
@@ -178,14 +178,14 @@ def update_ticket_tanggal(contact):
 def update_ticket_gate():
     print_header("MENGUPDATE INFO KONTAK")
     print(f"Studio Lama : {ticket}")
-    new_studio = input("Masukkan Studio baru : ")
+    new_studio = input("Masukkan gate baru : ")
     respon = input("Apakah yakin data ingin diubah (Y/N) : ")
     result = verify_ans(respon)
-    if result :
+    if result:
         tickets[new_studio] = tickets[ticket]
         del tickets[ticket]
         print("Data Telah di simpan")
-        print_data(new_studio)
+        print_data(new_gste)
     else:
         print("Data Batal diubah")
         saved = save_data_contacts()
@@ -193,12 +193,8 @@ def update_ticket_gate():
             print("Data Kontak Telah di-update.")
         else:
             print("Kesalahan saat menyimpan")
- 
-    else:
-        print("Data Tidak Ada")
-    input("Tekan ENTER untuk kembali ke MENU")
- 
- def update_ticket_nomor(contact):
+
+def update_ticket_nomor(ticket):
     print(f"Nomor Kursi Lama : {ticket}")
     new_chair = input("Masukkan Nomor Kursi baru : ")
     respon = input("Apakah yakin data ingin diubah (Y/N) : ")
@@ -217,7 +213,7 @@ def update_ticket():
     exists = searching_by_id(nopes)
     if exists:
         print_data(id_pesanan)
-        print("EDIT FIELD [1] JUDUL - [2] JAM - [3] TANGGAL - [4] STUDIO - [5] NOMOR KURSI")
+        print("EDIT FIELD [1] JUDUL - [2] JAM - [3] TANGGAL - [4] GATE - [5] NOMOR KURSI")
         respon = input("MASUKAN PILIHAN (1/2/3) : ")
         if respon == "1":
             update_ticket_judul(nopes)
@@ -250,7 +246,7 @@ def check_user_input(char):
         print("BYE!!!")
         return True 
     elif char == "1":
-        view_contacts()
+        view_()
     elif char == "2":
         add_contact()
     elif char == "3":
@@ -273,7 +269,7 @@ def save_data_tickets():
     return True
 
 stop = False
-file_path = "filetxt/tickets.json"
+file_path = "data/ticket_table.json"
 tickets = load_data_tickets()
 while not stop:
     print_menu()
